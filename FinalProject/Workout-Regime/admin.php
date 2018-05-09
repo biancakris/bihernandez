20cf8d5c2f7498a87171633eafdd30983bc21ec5
Bianca Hernandez <bihernandez@csumb.edu>
2018-05-09 22:39:59 +0000

    renamed file

--- FinalProject/Workout-Regime/admin.php
+++ FinalProject/Workout-Regime/admin.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <link rel = "stylesheet" href = "css/styles.css"> 
+        <link href="https://fonts.googleapis.com/css?family=Lora:700|Scope+One|Arvo|Slabo+27px" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <title> Admin </title>
     </head>
     <body>
-
+    <center>
+        <div id = 'header' style = "">
+            <img src = "https://images.clipartuse.com/b6504904f5e194032e61f36898164b42_muscle-arm-clip-art-6-pack-abs-are-still-possible-this-summer-_400-400.png" style = "width:75px; height:75px;;">
+            Body Buddy
+            <img src = "https://images.clipartuse.com/b6504904f5e194032e61f36898164b42_muscle-arm-clip-art-6-pack-abs-are-still-possible-this-summer-_400-400.png" style = "width:75px; height:75px; transform: scaleX(-1)">
+           <div id = 'headSubtitle'>
+               Be Ready to Get Sweaty!!
+           </div> 
+        </div>
+        </center>
+       
+        <!--Body-->
+        <div>
+            <img id = "man" src = "https://s-media-cache-ak0.pinimg.com/originals/09/3d/02/093d0274d12253d5540c82b708e72e1a.png" style = "width:350px; ">
+            <center>
+            <div id = 'text' style = "width: 50%; text-align: center;">
+                   Admin Page
+            </div>
+            </center>
+            <img id = "woman" src = "http://vignette2.wikia.nocookie.net/animatedmusclewomen/images/b/b8/Amber_by_thefranksterchannel-da8ys4n.png/revision/latest?cb=20160705163233" style = "height: 450px; "> 
+        </div>
     </body>
 </html>
